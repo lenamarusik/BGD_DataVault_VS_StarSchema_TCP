1fe416732fb0f3e98c3814d00ac5aa9933646b1c
Maks Białas <maks.bialas@gmail.com>
2026-06-08 10:16:02 +0200

    feat: add ghost rows for customers and items

--- datavault_vs_star/models/data_vault/hubs/hub_customers.sql
+++ datavault_vs_star/models/data_vault/hubs/hub_customers.sql
@@ -6,9 +6,21 @@ WITH source_keys AS (
         'tpcds.customer' AS record_source
     FROM {{ ref("stg_customers") }}
     WHERE c_customer_id IS NOT NULL
+),
+
+ghost as (
+    SELECT
+        {{ ghost_key() }} AS customer_hk,
+        cast(null AS varchar) AS c_customer_id,
+        {{ ghost_load_date() }} AS load_date,
+        'SYSTEM' AS record_source
 )
 
-SELECT * FROM source_keys
+SELECT * FROM (
+    SELECT * FROM source_keys
+    UNION ALL
+    SELECT * FROM ghost
+)
 
 {% if is_incremental() %}
 WHERE customer_hk NOT IN (SELECT customer_hk FROM {{ this }})
