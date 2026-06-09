6c2a0c1bbe4a028610997831716d7fde1a1a5660
Maks Białas <maks.bialas@gmail.com>
2026-06-09 17:26:50 +0200

    feat: add sf parameter in generate bash script

--- generate_tpcds.sh
+++ generate_tpcds.sh
@@ -1,1 +1,2 @@
-duckdb datavault_vs_star/datavault_vs_star.duckdb -c "install tpcds; load tpcds; call dsdgen(sf=1)"
+SF=${1:-1}
+duckdb datavault_vs_star/datavault_vs_star.duckdb -c "install tpcds; load tpcds; call dsdgen(sf=${SF})"
